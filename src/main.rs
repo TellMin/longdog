c0f6d6c446ccd9f0be10e829e04f14e0a0b2ddd5
TellMin <dhgirkcls@gmail.com>
2024-01-18 21:00:17 +0900

    Add dependencies for clap crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+/// Simple program to generate long dog
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// How long should the dog be?
+    #[arg(short, long, default_value_t = 1)]
+    long: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    let long_dog = format!("L{}ong dog", "o".repeat(args.long as usize));
+    println!("{}", long_dog);
 }
