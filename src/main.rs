165e2ddecd4aafefb0858660d06a6b550d7815b2
TellMin <dhgirkcls@gmail.com>
2024-01-20 01:03:42 +0900

    Add Dog struct and related methods

--- src/main.rs
+++ src/main.rs
@@ -11,16 +11,61 @@ struct Args {
     long: u8,
 }
 
+struct Dog {
+    head: ImageBuffer<Rgba<u8>, Vec<u8>>,
+    body: ImageBuffer<Rgba<u8>, Vec<u8>>,
+    tail: ImageBuffer<Rgba<u8>, Vec<u8>>,
+    long: u8,
+}
+
+impl Dog {
+    fn new<P: AsRef<Path>>(head: P, body: P, tail: P, long: u8) -> Dog {
+        Dog {
+            head: load_image(head).unwrap(),
+            body: load_image(body).unwrap(),
+            tail: load_image(tail).unwrap(),
+            long,
+        }
+    }
+
+    fn width(&self) -> u32 {
+        self.head.width() + self.body.width() * (self.long) as u32 + self.tail.width()
+    }
+
+    fn height(&self) -> u32 {
+        self.head.height()
+    }
+
+    fn compose(&self, long: u8) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+        let (width, height) = (self.width(), self.height());
+        let mut img = ImageBuffer::new(width, height);
+
+        let mut x = 0;
+        img.copy_from(&self.head, x, 0).unwrap();
+        x += self.head.width();
+
+        for _ in 0..long {
+            img.copy_from(&self.body, x, 0).unwrap();
+            x += self.body.width();
+        }
+
+        img.copy_from(&self.tail, x, 0).unwrap();
+
+        img
+    }
+}
+
 fn main() -> Result<(), ImageError> {
     let args = Args::parse();
 
-    let images = load_images(&[
+    let dog = Dog::new(
         "images/data01.png",
         "images/data02.png",
         "images/data03.png",
-    ])?;
+        args.long,
+    );
 
-    let img = conpose_images(&images, args.long);
+    let img = dog.compose(args.long);
 
     save_image(
         &img,
@@ -31,35 +76,10 @@ fn main() -> Result<(), ImageError> {
     Ok(())
 }
 
-fn load_images<P: AsRef<Path>>(
-    paths: &[P],
-) -> Result<Vec<ImageBuffer<Rgba<u8>, Vec<u8>>>, ImageError> {
-    paths
-        .iter()
-        .map(|path| image::open(path).map(|img| img.to_rgba8()))
-        .collect()
-}
-
-fn conpose_images(
-    images: &[ImageBuffer<Rgba<u8>, Vec<u8>>],
-    long: u8,
-) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
-    let (width, height) = (
-        images.iter().map(|img| img.width()).sum(),
-        images.iter().map(|img| img.height()).max().unwrap(),
-    );
-
-    let mut img = ImageBuffer::new(width, height);
-
-    let mut x = 0;
-    for i in 0..long {
-        img.copy_from(&images[i as usize], x, 0).unwrap();
-        x += images[i as usize].width();
-    }
-
-    img
-}
-
 fn save_image(img: &ImageBuffer<Rgba<u8>, Vec<u8>>, filename: &str) -> Result<(), ImageError> {
     img.save(filename)
+}
+
+fn load_image<P: AsRef<Path>>(path: P) -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>, ImageError> {
+    image::open(path).map(|img| img.to_rgba8())
 }
